f3101a3f0bd2d7c5c5333c92fc56ec1e89c477ec
Nmodern <noxsweatxnoxshirtx@hotmail.com>
2026-05-27 11:48:34 -0700

    practice java statements w/ comments

---
+++ practice/StatementsWComments.java
@@ -0,0 +1,24 @@
+public class StatementsWComments {
+	public static void main(String[] args){
+		System.out.println("Try me");
+		//Well, I don't necessarily mean so. 
+		/*
+		I understand what I'm expected to say and do 
+		I mean, to some degree, the first thing shouldn't 
+		be "hey, you, try me". That's not at all what should 
+		ever should be the first impression anyone gets, but the 
+		more I neglect to say something the more I feel I inevitaby 
+		come across as inconsiderate or violently provocative. 
+		*/
+		System.out.print("Try me without the ln");
+		/*
+		I know that the new line command is /n. 
+		Now, if only I could have remembered that when trying to 
+		compile the films of my early childhood. I should just 
+		print out the word s-u-s and be done with it...I think HTML 
+		would be better in that case...14px; 12px; 6px; 2px; 
+		sus. 
+		*/
+		System.out.println("/nTry me with a New Line");
+	}
+}
