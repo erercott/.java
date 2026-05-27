743e73809d72d85b53a41188854572e5cf663c14
Nmodern <noxsweatxnoxshirtx@hotmail.com>
2026-05-27 10:38:25 -0700

    To add print numbers in java

---
+++ test/PrintNumbers.java
@@ -0,0 +1,9 @@
+public class PrintNumbers {
+	
+	public static void main(String[] args){
+		System.out.println(3 + 3);
+		System.out.println(2 * 5);
+		System.out.println(358);
+		System.out.println(50000);
+	}
+}
