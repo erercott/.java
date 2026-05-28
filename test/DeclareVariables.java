c0f10b26a9e43ec49de2fea4efafa28507a10f92
Nmodern <noxsweatxnoxshirtx@hotmail.com>
2026-05-28 12:33:52 -0700

    int variables in java

---
+++ test/DeclareVariables.java
@@ -0,0 +1,9 @@
+public class DeclareVariables {
+	public static void main(String[] args){
+		int x = 10; 
+		int y = 20; 
+		int z = 30; 
+		
+		System.out.println(x + y + z);
+	}
+}
---
+++ test/OneValueMultipleVariables.java
@@ -0,0 +1,8 @@
+public class OneValueMultipleVariables {
+	public static void main(String[] args){
+	int x, y; 
+	x = y = 100; 
+	
+	System.out.println(x + y);	
+	}
+}
