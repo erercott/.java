22f9f0bd91ff07b83b02a40a36e72b55b299e5a2
Nmodern <noxsweatxnoxshirtx@hotmail.com>
2026-06-01 12:22:56 -0700

    add String variables in file

---
+++ test/variables/String.java
@@ -0,0 +1,13 @@
+public class String { 
+	public static void main(String[] args) {
+		
+		String Phillip$Drummond = "Conrad Bain"; 
+		String Arnold$Jackson = "Gary Coleman";
+		String Willis$Jackson = "Todd Bridges";
+		String Kimberly$Drummond = "Dana Plato";
+		String Charolette$Rae = "Edna Garrett";
+		String Pearl$Gallagher = "Mary Jo Catlett";
+		String Maggie$McKinney$Drummond = "Dixie Carter";
+		
+	}
+}
