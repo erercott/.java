dfaae97349c98c88abac85fde988306c5c79fe90
Nmodern <noxsweatxnoxshirtx@hotmail.com>
2026-06-01 14:43:50 -0700

    Populate file with undeclared variables

---
+++ test/variables/UndeclaredVariables.java
@@ -0,0 +1,12 @@
+public class UndeclaredVariables {
+	public static void main(String[] args){
+		
+		int YearOfRelease;
+		String AlbumName;  
+		int track; 
+		
+		
+		track = 2; 
+		System.out.println(track);
+	}
+}
