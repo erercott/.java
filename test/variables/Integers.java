945450eb30f394ca74559f2d0bef36e6428b8ff6
Nmodern <noxsweatxnoxshirtx@hotmail.com>
2026-06-01 12:58:53 -0700

    Add integers to file in variables folder

---
+++ test/variables/Integers.java
@@ -0,0 +1,18 @@
+public class Integers {
+	
+	public static void main(String[] args) {
+		int twenty_six = 1999;
+		int twenty_seven = 1998;
+		int twenty_eight = 1997;
+		int twenty_nine = 1996;
+		int thirty = 1995;
+		int thirty_one = 1994;
+		int thirty_two = 1993;
+		int thirty_three = 1992;
+		int thirty_four = 1991;
+		int thirty_five - 1990;
+		
+		System.out.println(thirty_five); 
+        System.out.println(twenty_six);
+	}
+}
